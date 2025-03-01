c3f9b17366eed9a8f052cedb8c835499ce5af872
minhdinh365 <minhdinhtran0@gmail.com>
2025-03-01 11:15:08 +0700

    fix bug

--- next.config.js
+++ next.config.js
@@ -3,7 +3,24 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/*",
+        source: "/sitemap.xml",
+        headers: [
+          {
+            key: "Referrer-Policy",
+            value: "no-referrer-when-downgrade",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, OPTIONS",
+          },
+        ],
+      },
+      {
+        source: "/robots.txt",
         headers: [
           {
             key: "Referrer-Policy",
