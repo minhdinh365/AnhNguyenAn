08b4d956a3e6f53fec76ed3089f36fb798838295
minhdinh365 <minhdinhtran0@gmail.com>
2025-03-08 03:04:41 +0700

    fix bug robots

--- app/robots.ts
+++ app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ["/admin", "/privacy"],
       },
     ],
-    sitemap: `"https://anhnguyenan.netlify.app/sitemap.xml`,
+    sitemap: "https://anhnguyenan.netlify.app/sitemap.xml",
   };
 }
